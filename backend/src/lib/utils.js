540c96572d979266808e751d32ad74d9cd0de2f8
Rahul Raj <rahul.r0644@gmail.com>
2025-01-04 21:10:39 +0530

    commit

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,8 +8,8 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
     httpOnly: true, // Prevent client-side access to the cookie
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", // CSRF protection
-    secure: process.env.NODE_ENV === "production", // Only sent over HTTPS
+    sameSite: process.env.NODE_ENV == "production" ? "none" : "strict", // CSRF protection
+    secure: process.env.NODE_ENV == "production" , // Only sent over HTTPS
   });
 
   return token;
