79dcf56dd117384f442c4618cb1aff6d8c7060db
Rahul Raj <rahul.r0644@gmail.com>
2025-01-07 21:52:50 +0530

    commit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import { useChatStore } from "../store/useChatStore";
-
 import Sidebar from "../components/Sidebar";
 import NoChatSelected from "../components/NoChatSelected";
 import ChatContainer from "../components/ChatContainer";
@@ -8,9 +7,9 @@ const HomePage = () => {
   const { selectedUser } = useChatStore();
 
   return (
-    <div className="h-screen bg-base-200">
-      <div className="flex items-center justify-center pt-20 px-7">
-        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-10xl h-[calc(100vh-5rem)]">
+    <div className="h-screen w-full bg-base-200">
+      <div className="flex items-center justify-center pt-20 w-full px-0 2lg:px-2">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-screen-xl h-[calc(100vh-5rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
             <Sidebar />
             {selectedUser ? <ChatContainer /> : <NoChatSelected />}
@@ -20,4 +19,5 @@ const HomePage = () => {
     </div>
   );
 };
+
 export default HomePage;
