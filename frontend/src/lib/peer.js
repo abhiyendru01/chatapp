7451f5b2501d5d74076686b77b6f6f458e50f5f0
Rahul Raj <rahul.r0644@gmail.com>
2025-02-18 21:23:55 +0530

    commit

---
+++ frontend/src/lib/peer.js
@@ -0,0 +1,23 @@
+import Peer from "simple-peer";
+
+export const createPeer = (userId, socket, stream) => {
+  const peer = new Peer({
+    initiator: true,
+    trickle: false,
+    stream,
+  });
+
+  peer.on("signal", (signal) => {
+    socket.emit("callUser", { userToCall: userId, signal });
+  });
+
+  return peer;
+};
+
+export const acceptPeer = (incomingSignal, socket, stream) => {
+  const peer = new Peer({
+    initiator: false,
+    trickle: false,    stream,
+  });  peer.signal(incomingSignal);
+  return peer;
+};
