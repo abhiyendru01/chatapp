7af1e73d9ad13e340a4d0b418d50c982f2b73fd6
Rahul Raj <rahul.r0644@gmail.com>
2025-02-24 07:15:08 +0530

    commit

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -3,6 +3,7 @@ import { io } from "socket.io-client";
 const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:5001";
 const socket = io(backendUrl, {
   withCredentials: true,
+  secure: true,
 });
 
 // Log connection status
